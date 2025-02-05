ec43055b0015bd0133e08dcaee9de6dd57873013
安 云 <81630016@qq.com>
2025-02-05 10:48:09 +0800

    第二节内容完成

--- 代码/语法基础课/第二节/second.cpp
+++ 代码/语法基础课/第二节/second.cpp
@@ -160,3 +160,184 @@
 //    }
 //    return 0;
 //}
+
+//657
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+//		cout << "Valores aceitos" << endl;
+//	else
+//		cout << "Valores nao aceitos" << endl;
+//	return 0;
+//}
+
+//671
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int x;
+//    cin >> x;
+//    if (x == 61)   
+//        cout << "Brasilia" << endl;
+//    else if (x == 71) 
+//        cout << "Salvador" << endl;
+//    else if (x == 11) 
+//        cout << "Sao Paulo" << endl;
+//    else if (x == 21) 
+//        cout << "Rio de Janeiro" << endl;
+//    else if (x == 32) 
+//        cout << "Juiz de Fora" << endl;
+//    else if (x == 19) 
+//        cout << "Campinas" << endl;
+//    else if (x == 27) 
+//        cout << "Vitoria" << endl;
+//    else if (x == 31) 
+//        cout << "Belo Horizonte" << endl;
+//    else 
+//        cout << "DDD nao cadastrado" << endl;
+//    return 0;
+//}
+
+//662
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	double x, y;
+//	cin >> x >> y;
+//	if (x == 0 && y == 0)
+//		cout << "Origem";
+//	else if (x == 0)
+//		cout << "Eixo Y";
+//	else if (y == 0)
+//		cout << "Eixo X";
+//	else if (x > 0 && y > 0)
+//		cout << "Q1";
+//	else if (x < 0 && y > 0)
+//		cout << "Q2";
+//	else if (x < 0 && y < 0)
+//		cout << "Q3";
+//	else if (x > 0 && y < 0)
+//		cout << "Q4";
+//	return 0;
+//}
+
+//666
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	double a, b, c;
+//	cin >> a >> b >> c;
+//	if(b > a)
+//		swap(a, b);
+//	if(c > a)
+//		swap(a, c);
+//	if(c > b)
+//		swap(b, c);
+//	if (a >= b + c)
+//		cout << "NAO FORMA TRIANGULO" << endl;
+//	else
+//	{
+//		if (a * a == b * b + c * c)
+//			cout << "TRIANGULO RETANGULO" << endl;
+//		else if (a * a > b * b + c * c)
+//			cout << "TRIANGULO OBTUSANGULO" << endl;
+//		else
+//			cout << "TRIANGULO ACUTANGULO" << endl;
+//		if (a == b && b == c)
+//			cout << "TRIANGULO EQUILATERO" << endl;
+//		else if (a == b ||  b == c)
+//			cout << "TRIANGULO ISOSCELES" << endl;
+//	}
+//	return 0;
+//}
+
+//668
+//法一（常规）：
+//#include<cstdio>
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	int start = a * 60 + b;
+//	int end = c * 60 + d;
+//	int spent_time = end - start;
+//	if (spent_time <= 0)
+//		spent_time = 24 * 60 - start + end;
+//	printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", spent_time / 60, spent_time % 60);
+//	return 0;
+//}
+// 
+//法二（预先设定差一天后用余数处理，需要注意时间差为24小时的时候取余会变为0，需要特殊处理一下）：
+//#include<cstdio>
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	b += a * 60;//转化成分钟数
+//	d += c * 60;
+//	a = (d - b + 24 * 60) % (24 * 60);//预先设定多一天的分钟数，通过取模得到经过的时间
+//	a = (a) ? (a) : (24 * 60);//若a为0，则过去了一天，特殊处理
+//	printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", a / 60, a % 60);
+//	return 0;
+//}
+
+//672
+//#include<iostream>
+//#include<cstdio>
+//using namespace std;
+//int main()
+//{
+//	double s;
+//	cin >> s;
+//	if (s <= 2000)
+//		cout << "Isento" << endl;
+//	else if (s <= 3000)
+//		printf("R$ %.2lf", (s - 2000) * 0.08);
+//	else if (s <= 4500)
+//		printf("R$ %.2lf", (3000 - 2000) * 0.08 + (s - 3000) * 0.18);
+//	else
+//		printf("R$ %.2lf", (3000 - 2000) * 0.08 + (4500 - 3000) * 0.18 + (s - 4500) * 0.28);
+//	return 0;
+//}
+
+//663
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int x, y, z, x1, y1, z1;
+//	cin >> x >> y >> z;
+//	x1 = x, y1 = y, z1 = z;
+//	if (x > y) swap(x, y);
+//	if (x > z) swap(x, z);
+//	if (y > z) swap(y, z);
+//	cout << x << endl << y << endl << z << endl << endl << x1 << endl << y1 << endl << z1 << endl;
+//	return 0;
+//}
+
+//658
+//#include<iostream>
+//#include<cstdio>
+//#include<cmath>
+//using namespace std;
+//int main()
+//{
+//	double a, b, c;
+//	cin >> a >> b >> c;
+//	if (a == 0 || b * b - 4 * a * c < 0)
+//		cout << "Impossivel calcular";
+//	else
+//		printf("R1 = %.5lf\nR2 = %.5lf", (-b + sqrt(b * b - 4 * a * c)) / (2 * a), (-b - sqrt(b * b - 4 * a * c)) / (2 * a));
+//	return 0;
+//}
